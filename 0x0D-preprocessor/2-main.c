ab4945557403c345a10280df51c7e035092f12fc
REBONE56 <rebonemakgati@gmail.com>
2023-09-12 16:33:40 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
+int main() {
+    printf("%s\n", __FILE__);
+    return 0;
 }
